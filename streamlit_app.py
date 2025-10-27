b76327d63a1e2ce3e8cff1a130446331e802a9d3
John Kim <junwon.kim92@gmail.com>
2025-10-26 21:23:27 -0400

    working multipage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
 st.title("SOPO Internal Tools")
-
 st.write("Select a tool from the left sidebar to get started")
+
+fooda_page = st.Page("pages/fooda.py", title="Fooda Label Generator")
+grubhub_page = st.Page("pages/grubhub.py", title="Grubhub Label Generator")
+grubhub_pdf_page = st.Page("pages/grubhub_pdf.py", title="Grubhub PDF Generator")
+
+pg = st.navigation([fooda_page, grubhub_page, grubhub_pdf_page])
+pg.run()
